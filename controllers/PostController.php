30992b10c657e4caacbeaf304759724575075774
Nikita <nik20z.yar@gmail.com>
2024-02-27 00:28:01 +0300

    Initial commit

---
+++ controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Post;
+use yii\data\Pagination;
+use yii\web\Controller;
+
+class PostController extends Controller
+{
+    
+    public function actionIndex()
+    {
+        // $posts = Post::find()->all();
+        // return $this->render('index', compact('posts'));
+        $query = Post::find()->with('category');
+        $pages = new Pagination([
+            'totalCount' => $query->count(), 
+            'pageSize' => 4,
+            'forcePageParam' => false,
+            'pageSizeParam' => false
+        ]);
+        $posts = $query->offset($pages->offset)->limit($pages->limit)->all();
+        return $this->render('index', compact('posts', 'pages'));
+    }
+}
---
+++ models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+
+class Post extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    public function getCategory()
+    {
+        return $this->hasOne(Category::class, ['id' => 'category_id']);
+    }
+}
